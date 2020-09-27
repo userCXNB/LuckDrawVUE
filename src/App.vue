1d81e6f7dab62294d1bd3450488f5d6cad400baa
cxNB123 <1148664901@qq.com>
2020-09-27 18:07:56 +0800

    tiji

--- src/App.vue
+++ src/App.vue
@@ -4,40 +4,54 @@
          <div class="core" style="margin-bottom:1px;">
            <nav class="set-nav">        
               <a href="https://data.gov.sg/" class="set-logo">
-                <img src="./assets/images/logo.png" style="width:150px;height:47px;" alt="Data.gov.sg" title="Data.gov.sg">
+                <img src="./assets/images/logo.jpg" alt="Data.gov.sg" title="Data.gov.sg">
               </a>
+              <a href="https://data.gov.sg/" class="set-title">Data.gov.sg</a>
               <ul>
                 <li>www1</li>
                 <li>www2</li>
                 <li>www3</li>
               </ul>
           </nav>
-          <!-- <section class="set-section" v-show="switchStatus"> -->
-          <section class="set-section" v-show="switchStatus" :style="'background:url('+cssSRC[activeIndex]+')'">
-                 <dd style="font:44px/70px '微软雅黑';color:#fff;margin-top: 84px;text-shadow:5px 2px 6px #000;">{{menuList[activeIndex].name}}</dd>
-                  <div style="margin-bottom:22px;">
-                    <el-input placeholder="请输入内容"  class="input-with-select" style="width:500px;">
-                      <el-button slot="append" icon="el-icon-search" style="height:54px;"></el-button>
+          <section class="set-section">
+                 <dd style="font-size:44px;">Transport</dd>
+                  <div style="margin-top: 15px;">
+                    <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
+                      <el-button slot="append" icon="el-icon-search"></el-button>
                     </el-input>
                   </div>
-                  <div>
-                         <el-button  type="primary" style="width:175px;" @click="switchBtn">view All Datasets</el-button>
-                  </div>
+
+                  <el-button  type="primary">view All Datasets</el-button>
 
           </section>
           <el-menu
-            v-if="switchStatus"
             :default-active="activeIndex"
-            class="el-menu-demo M_top_350"
+            class="el-menu-demo"
             mode="horizontal"
             @select="handleSelect"
-            background-color="#f5f5f5"
-            text-color="#113355"
+            background-color="#545c64"
+            text-color="#fff"
             active-text-color="#ffd04b">
-            <el-menu-item v-for="(item,index) in menuList" :key="index" :index="index" style="text-align:center;"><span :class="'iconfont icon-'+item.icon" style="top:-27px;left:16px;position:relative;font-size:27px;line-height: 99px;
-}"></span><span style="position:relative;left:-12px;">{{item.name}}</span></el-menu-item>
+            <el-menu-item index="1">处理中心</el-menu-item>
+            <el-submenu index="2">
+              <template slot="title">我的工作台</template>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
+              <el-menu-item index="2-3">选项3</el-menu-item>
+              <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                <el-menu-item index="2-4-3">选项3</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="3" disabled>消息中心</el-menu-item>
+            <el-menu-item index="4">订单管理</el-menu-item>
           </el-menu>
+
           <router-view/>
+
+
         </div>
 
       </div>
@@ -67,69 +81,39 @@
 
 </template>
 <script>
-import {mapActions,mapState,mapGetters} from "vuex";
 export default {
   name: 'App',
     data () {
     return {
-        activeIndex:0,
-        cssSRC:[require("./assets/images/banner.jpg"),require("./assets/images/banner1.jpg")],
-        menuList:[
-          {name:"经济",icon:"jingji"},
-          {name:"环境",icon:"huanjing"},
-          {name:"教育",icon:"education_icon"},
-          {name:"就业",icon:"jiuyechuangye"},
-          {name:"交通",icon:"traffic"},
-          {name:"安全",icon:"anquan"},
-          {name:"文化",icon:"wenhuachuanmei"},
-          {name:"卫生",icon:"huanjing"},
-          {name:"市场监控",icon:"zhongxinhuanjing"}
-        ]
+        activeIndex: '1',
     }
   },
-  computed: {
-            ...mapState([
-                'switchStatus'
-            ]),
-        }, 
   methods: {
-      ...mapActions(['setSwitchStatus']),
-      ...mapGetters(['getSwitchStatus']),
       handleSelect(key, keyPath) {
         console.log(key, keyPath);
-        this.activeIndex = key
-      },
-      menuOpt(){
-          
-      },
-      switchBtn(){
-          this.setSwitchStatus()
-          this.$router.push({  //核心语句
-                path:'/home'
-                })
+      }
     }
-  },
-  mounted(){
-  } 
 }
 </script>
-<style lang="less">
+<style lang="less" scoped>
   
   #app{
     
     display:flex;
     justify-content:center;
     .core{
+      background: red;
        width:1050px;
     }
   }
   .set-nav{
     height:47px;
+    background: gray;
+    // margin-bottom: 350px;
     .set-logo{
        float:left;
        display:flex;
        align-items: center;
-       margin-right: 14px;
        height:47px;      
     }
     .set-title{
@@ -150,30 +134,24 @@ export default {
     width: 100%;
     background:url(./assets/images/banner.jpg);
     position:absolute;
-    display:flex;
-    flex-direction:column;
     left:0;
     top:47px;
     right:0;
     margin:0 auto;
     height:350px;
-    text-align:center;
 
   }
-  .el-input__inner {
-              height: 54px!important;
-              }
-  .M_top_350{
-    margin-top:350px!important;
+  .el-menu-demo{
+    margin-top:350px;
   }
   .set-footer{
     display:flex;
     justify-content: center;
     background:#113355;
-    color:#fff;
     .footer-main{
       height:340px;
       padding-top:33px;
+      background: red;
        width:1050px;
        .firstul{
          float:left;
